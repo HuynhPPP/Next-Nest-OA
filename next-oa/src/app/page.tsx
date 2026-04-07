7092a26b16ff7e2b950c46f634c3b5e3b0a11a1b
HuynhPPP <tankhuong02l@gmail.com>
2026-04-07 21:38:43 +0700

    first commit with both nest-oa and next-oa

---
+++ next-oa/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+
+export default function Home() {
+  return (
+    <div>
+      <div>Hello World
+        <div>
+          <Button type="primary">Click me</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
