8c6459b592c7f803072631b0f898299b28dccbfe
HuynhPPP <tankhuong02l@gmail.com>
2026-04-10 14:50:33 +0700

    creat API CURD users, create, get

--- nest-oa/src/modules/users/schemas/user.schema.ts
+++ nest-oa/src/modules/users/schemas/user.schema.ts
@@ -23,14 +23,14 @@ export class User {
     @Prop()
     image: string;
 
-    @Prop()
+    @Prop({ default: 'users' })
     role: string;
 
-    @Prop()
+    @Prop({ default: 'local' })
     accountType: string;
 
-    @Prop()
-    isActive: string;
+    @Prop({ default: false })
+    isActive: boolean;
 
     @Prop()
     codeId: string;
--- nest-oa/src/modules/users/users.service.ts
+++ nest-oa/src/modules/users/users.service.ts
@@ -1,15 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { hashPasswordHelper } from '@/helpers/utils';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name)
+  private userModel: Model<User>,
+  ) { }
+
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.exists({ email });
+    if (user) return true;
+    return false;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+
+    // check email
+    const isEmailExist = await this.isEmailExist(email);
+    if (isEmailExist === true) {
+      throw new BadRequestException(`Email ${email} đã tồn tại. Vui lòng sử dụng email khác`);
+    }
+
+    // hash password
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      image,
+    });
+    return {
+      _id: user._id,
+    };
+  }
+
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+    if(filter.current) {
+      current = filter.current;
+      delete filter.current;
+    }
+    if(filter.pageSize) {
+      pageSize = filter.pageSize;
+      delete filter.pageSize;
+    }
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const skip = (current - 1) * pageSize;
+
+    const result = await this.userModel
+      .find(filter)
+      .sort(sort as any)
+      .skip(skip)
+      .select('-password')
+      .limit(pageSize)
+
+    return {result, totalPages};
   }
 
   findOne(id: number) {
